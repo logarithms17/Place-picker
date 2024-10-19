9d1bfca4cd24a111d40371b0638be4e637e295eb
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-10-19 23:53:08 +0800

    handling error

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import axios from "axios";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setLoading(true);
@@ -12,15 +14,26 @@ export default function AvailablePlaces({ onSelectPlace }) {
       try {
         const result = await axios.get("http://localhost:3000/places");
         setAvailablePlaces(result.data.places);
-        setLoading(false);
       } catch (error) {
         console.log(error);
+        setError(error);
       }
+      setLoading(false);
     };
 
     fetchData();
   }, []);
 
+  if (error) {
+    return (
+      <Error
+        title="An error occured!"
+        message={
+          error.message || "Could not fetch any places, please try again"
+        }
+      />
+    );
+  }
   return (
     <Places
       title="Available Places"
